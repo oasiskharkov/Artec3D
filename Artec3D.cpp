5fe362f5213d0e3bc32cf1f20655d2f1009f036d
oasiskharkov <kasystems@yandex.ru>
2021-08-28 11:43:43 +0300

    Sixth commit

--- Artec3D.cpp
+++ Artec3D.cpp
@@ -30,11 +30,23 @@ void readValuesFromFile(const std::string& fileName, std::vector<unsigned>& valu
 // get total file length
 const long long getFileLength(const std::string& fileName);
 
-// merge sorted vectors
+// merge two sorted vectors
 std::vector<unsigned> mergeSortedVectors(const std::vector<unsigned>& v1, const std::vector<unsigned>& v2);
+
+// fill vectors from input binary file
+void fillVectorsFromFile(const std::string& fileName, std::vector<std::vector<unsigned>>& values, const long long fileLength);
 
 // sort subsequence
 void sortSubsequence(std::vector<unsigned>& result, const std::vector<std::vector<unsigned>>& values, const size_t left, const size_t right);
+
+// write sorted values to binary file
+void writeSortedVectorToFile(const std::string& fileName, const std::vector<unsigned>& result);
+
+// merge all sorted vectors to result sorted vector
+void mergeAllSortedVectors(std::vector<unsigned>& result, std::vector<std::vector<unsigned>>& values);
+
+// sort all vectors
+void sortVectors(std::vector<std::vector<unsigned>>& values);
 
 int main(int argc, char* argv[])
 {
@@ -52,86 +64,22 @@ int main(int argc, char* argv[])
          throw std::logic_error("Can't open input binary file to write random values.");
       }*/
 
-      const long long fileLenght = getFileLength(argv[1]);
-      if (!fileLenght)
+      const long long fileLength = getFileLength(argv[1]);
+      if (!fileLength)
       {
          throw std::logic_error("Input file is empty.");
       }
 
-      const long long size = fileLenght % segmentSize == 0 ? fileLenght / segmentSize : fileLenght / segmentSize + 1;
+      const long long size = fileLength % segmentSize == 0 ? fileLength / segmentSize : fileLength / segmentSize + 1;
       std::vector<std::vector<unsigned>> values(size);
-      int counter = 0;
-      long long startPosition = 0;
-      while (startPosition < fileLenght)
-      {
-         std::vector<std::unique_ptr<std::thread>> threads;
-         for (int i = 0; i < maxThreadsCount; ++i)
-         {
-            if (startPosition >= fileLenght)
-            {
-               break;
-            }
-            const long long segment = fileLenght - startPosition >= segmentSize ? segmentSize : fileLenght - startPosition;
-            threads.emplace_back(std::make_unique<std::thread>(std::bind(&readValuesFromFile, std::cref(argv[1]), std::ref(values[counter]), startPosition, segment)));
-            startPosition += segment;
-            counter++;
-         }
+      fillVectorsFromFile(argv[1], values, fileLength);
 
-         for (auto& vp : threads)
-         {
-            if (vp->joinable())
-            {
-               vp->join();
-            }
-         }
-      }
+      sortVectors(values);
 
-      counter = 0;
-      while (counter < size)
-      {
-         std::vector<std::unique_ptr<std::thread>> threads;
-         for (int i = 0; i < maxThreadsCount; ++i)
-         {
-            if (counter == size)
-            {
-               break;
-            }
-            threads.emplace_back(std::make_unique<std::thread>([&vec = values[counter]](){ std::sort(vec.begin(), vec.end()); }));
-            counter++;
-         }
+      std::vector<unsigned> result;
+      mergeAllSortedVectors(result, values);
 
-         for (auto& vp : threads)
-         {
-            if (vp->joinable())
-            {
-               vp->join();
-            }
-         }
-      }
-
-      size_t start = 0;
-      size_t middle = values.size() / 2;
-      size_t end = values.size();
-
-      std::vector<unsigned> left(0);
-      std::vector<unsigned> right(0);
-      std::unique_ptr<std::thread> leftThread = std::make_unique<std::thread>(&sortSubsequence, std::ref(left), std::cref(values), start, middle);
-      std::unique_ptr<std::thread> rightThread = std::make_unique<std::thread>(&sortSubsequence, std::ref(right), std::cref(values), middle, end);
-
-      leftThread->join();
-      rightThread->join();
-
-      std::vector<unsigned> result = mergeSortedVectors(left, right);
-
-      std::ofstream out(argv[2], std::ios::binary);
-      if (!out.is_open())
-      {
-         throw std::ios::failure("Can't open output file to write sorted values.");
-      }
-      //std::copy(std::begin(result), std::end(result), std::ostream_iterator<int>(out, " "));
-      out.write(reinterpret_cast<char*>(result.data()), sizeof(unsigned) * result.size());
-      out.close();
-      
+      writeSortedVectorToFile(argv[2], result);
    }
    catch (const std::exception& ex)
    {
@@ -211,5 +159,90 @@ void sortSubsequence(std::vector<unsigned>& result, const std::vector<std::vecto
    for (size_t i = left; i < right; ++i)
    {
       result = mergeSortedVectors(result, values[i]);
+   }
+}
+
+void writeSortedVectorToFile(const std::string& fileName, const std::vector<unsigned>& result)
+{
+   std::ofstream out(fileName, std::ios::binary);
+   if (!out.is_open())
+   {
+      throw std::ios::failure("Can't open output file to write sorted values.");
+   }
+   //std::copy(std::begin(result), std::end(result), std::ostream_iterator<int>(out, " "));
+   out.write(reinterpret_cast<const char*>(result.data()), sizeof(unsigned) * result.size());
+   out.close();
+}
+
+void mergeAllSortedVectors(std::vector<unsigned>& result, std::vector<std::vector<unsigned>>& values)
+{
+   size_t start = 0;
+   size_t middle = values.size() / 2;
+   size_t end = values.size();
+
+   std::vector<unsigned> left(0);
+   std::vector<unsigned> right(0);
+   std::unique_ptr<std::thread> leftThread = std::make_unique<std::thread>(&sortSubsequence, std::ref(left), std::cref(values), start, middle);
+   std::unique_ptr<std::thread> rightThread = std::make_unique<std::thread>(&sortSubsequence, std::ref(right), std::cref(values), middle, end);
+
+   leftThread->join();
+   rightThread->join();
+   values.clear();
+
+   result = mergeSortedVectors(left, right);
+}
+
+void sortVectors(std::vector<std::vector<unsigned>>& values)
+{
+   int counter = 0;
+   while (counter < values.size())
+   {
+      std::vector<std::unique_ptr<std::thread>> threads;
+      for (int i = 0; i < maxThreadsCount; ++i)
+      {
+         if (counter == values.size())
+         {
+            break;
+         }
+         threads.emplace_back(std::make_unique<std::thread>([&vec = values[counter]](){ std::sort(vec.begin(), vec.end()); }));
+         counter++;
+      }
+
+      for (auto& vp : threads)
+      {
+         if (vp->joinable())
+         {
+            vp->join();
+         }
+      }
+   }
+}
+
+void fillVectorsFromFile(const std::string& fileName, std::vector<std::vector<unsigned>>& values, const long long fileLength)
+{
+   int counter = 0;
+   long long startPosition = 0;
+   while (startPosition < fileLength)
+   {
+      std::vector<std::unique_ptr<std::thread>> threads;
+      for (int i = 0; i < maxThreadsCount; ++i)
+      {
+         if (startPosition >= fileLength)
+         {
+            break;
+         }
+         const long long segment = fileLength - startPosition >= segmentSize ? segmentSize : fileLength - startPosition;
+         threads.emplace_back(std::make_unique<std::thread>(std::bind(&readValuesFromFile, std::cref(fileName), std::ref(values[counter]), startPosition, segment)));
+         startPosition += segment;
+         counter++;
+      }
+
+      for (auto& vp : threads)
+      {
+         if (vp->joinable())
+         {
+            vp->join();
+         }
+      }
    }
 }
