dc8bc07d9fc02a49e32fadc8f2bc6d20428f76f4
oasiskharkov <kasystems@yandex.ru>
2021-09-01 15:42:39 +0300

    Initial commit to develop

--- Artec3D.cpp
+++ Artec3D.cpp
@@ -7,45 +7,39 @@
 #include <functional>
 #include <climits>
 #include <chrono>
+#include <unordered_map>
+#include <map>
+#include <mutex>
 
 // input binary file size in bytes 
-constexpr int generatedFileSize = 200 * 1024 * 1024 + 1024; 
+constexpr long long generatedFileSize = 2097153024;
 
 // command line arguments count
 constexpr int maxArgumentsCount = 3;
 
 // read segment size
-constexpr int segmentSize = 32 * 1024 * 1024;
+constexpr int segmentSize = 16 * 1024 * 1024;
 
 // max threads count
-constexpr int maxThreadsCount = 16;
+constexpr int maxThreadsCount = 6;
+
+// mutex
+std::mutex mtx;
 
 // generate input file
 int generateBinaryFile(const std::string& fileName);
 
-// read values from input file and add them to vector
-void readValuesFromFile(const std::string& fileName, std::vector<unsigned>& values, const long long startPosition, const long long segment);
-
 // get total file length
 const long long getFileLength(const std::string& fileName);
 
-// merge two sorted vectors
-std::vector<unsigned> mergeSortedVectors(const std::vector<unsigned>& v1, const std::vector<unsigned>& v2);
+// read values from input file and add them to unordered map
+void readValuesFromFile(const std::string& fileName, std::unordered_map<unsigned, int>& values, const long long startPosition, const long long segment);
 
-// fill vectors from input binary file
-void fillVectorsFromFile(const std::string& fileName, std::vector<std::vector<unsigned>>& values, const long long fileLength);
-
-// sort subsequence
-void sortSubsequence(std::vector<unsigned>& result, const std::vector<std::vector<unsigned>>& values, const size_t left, const size_t right);
+// fill unordered map with values and their quantity
+void fillMapWithUnsortedValues(const std::string& fileName, std::unordered_map<unsigned, int>& values, const long long fileLength);
 
 // write sorted values to binary file
-void writeSortedVectorToFile(const std::string& fileName, const std::vector<unsigned>& result);
-
-// merge all sorted vectors to result sorted vector
-void mergeAllSortedVectors(std::vector<unsigned>& result, std::vector<std::vector<unsigned>>& values);
-
-// sort all vectors
-void sortVectors(std::vector<std::vector<unsigned>>& values);
+void writeSortedValuesToFile(const std::string& fileName, const std::map<unsigned, int>& result);
 
 int main(int argc, char* argv[])
 {
@@ -69,16 +63,14 @@ int main(int argc, char* argv[])
          throw std::logic_error("Input file is empty.");
       }
 
-      const long long size = fileLength % segmentSize == 0 ? fileLength / segmentSize : fileLength / segmentSize + 1;
-      std::vector<std::vector<unsigned>> values(size);
-      fillVectorsFromFile(argv[1], values, fileLength);
+      std::unordered_map<unsigned, int> unsortedValues;
+      fillMapWithUnsortedValues(argv[1], unsortedValues, fileLength);
 
-      sortVectors(values);
+      std::map<unsigned, int> sortedValues;
+      sortedValues.insert(unsortedValues.begin(), unsortedValues.end());
+      unsortedValues.clear();
 
-      std::vector<unsigned> result;
-      mergeAllSortedVectors(result, values);
-
-      writeSortedVectorToFile(argv[2], result);
+      writeSortedValuesToFile(argv[2], sortedValues);
    }
    catch (const std::exception& ex)
    {
@@ -93,7 +85,7 @@ int main(int argc, char* argv[])
 
    auto end = std::chrono::steady_clock::now();
    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(end - begin);
-   std::cout << "Total time: " << seconds.count() << "sec." << std::endl;
+   std::cout << "Total time: " << seconds.count() << "s" << std::endl;
   
    return EXIT_SUCCESS;
 }
@@ -117,7 +109,7 @@ int generateBinaryFile(const std::string& fileName)
    return EXIT_FAILURE;
 }
 
-void readValuesFromFile(const std::string& fileName, std::vector<unsigned>& values, const long long startPosition, const long long segment)
+void readValuesFromFile(const std::string& fileName, std::unordered_map<unsigned, int>& values, const long long startPosition, const long long segment)
 {
    std::fstream in(fileName, std::ios::binary | std::ios::in);
    if (!in.is_open())
@@ -126,9 +118,19 @@ void readValuesFromFile(const std::string& fileName, std::vector<unsigned>& valu
    }
 
    in.seekg(startPosition);
-   values.resize(segment / sizeof(unsigned));
-   in.read(reinterpret_cast<char*>(values.data()), sizeof(unsigned) * values.size());
+   std::vector<unsigned> numbers(segment / sizeof(unsigned));
+   in.read(reinterpret_cast<char*>(numbers.data()), segment);
    in.close();
+
+   for (const auto& n : numbers)
+   {
+      std::unique_lock<std::mutex> ulock(mtx);
+      auto [iterator, success] = values.try_emplace(n, 1);
+      if (!success)
+      {
+         iterator->second++;
+      }
+   }
 }
 
 const long long getFileLength(const std::string& fileName)
@@ -146,87 +148,25 @@ const long long getFileLength(const std::string& fileName)
    return length;
 }
 
-std::vector<unsigned> mergeSortedVectors(const std::vector<unsigned>& v1, const std::vector<unsigned>& v2)
-{
-   std::vector<unsigned> result(v1.size() + v2.size());
-   std::merge(v1.begin(), v1.end(), v2.begin(), v2.end(), result.begin());
-   return result;
-}
-
-void sortSubsequence(std::vector<unsigned>& result, const std::vector<std::vector<unsigned>>& values, const size_t left, const size_t right)
-{
-   for (size_t i = left; i < right; ++i)
-   {
-      result = mergeSortedVectors(result, values[i]);
-   }
-}
-
-void writeSortedVectorToFile(const std::string& fileName, const std::vector<unsigned>& result)
+void writeSortedValuesToFile(const std::string& fileName, const std::map<unsigned, int>& values)
 {
    std::ofstream out(fileName, std::ios::binary);
    if (!out.is_open())
    {
       throw std::ios::failure("Can't open output file to write sorted values.");
    }
-   //std::copy(std::begin(result), std::end(result), std::ostream_iterator<int>(out, " "));
-   out.write(reinterpret_cast<const char*>(result.data()), sizeof(unsigned) * result.size());
+
+   for (const auto& vp : values)
+   {
+      std::vector<unsigned> result(vp.second, vp.first);
+      out.write(reinterpret_cast<const char*>(result.data()), sizeof(unsigned) * result.size());
+      //std::copy(result.begin(), result.end(), std::ostream_iterator<unsigned>(out, " "));
+   }
    out.close();
 }
 
-void mergeAllSortedVectors(std::vector<unsigned>& result, std::vector<std::vector<unsigned>>& values)
+void fillMapWithUnsortedValues(const std::string& fileName, std::unordered_map<unsigned, int>& values, const long long fileLength)
 {
-   size_t start = 0;
-   size_t middle = values.size() / 2;
-   size_t end = values.size();
-
-   std::vector<unsigned> left(0);
-   std::vector<unsigned> right(0);
-   std::unique_ptr<std::thread> leftThread = std::make_unique<std::thread>(&sortSubsequence, std::ref(left), std::cref(values), start, middle);
-   std::unique_ptr<std::thread> rightThread = std::make_unique<std::thread>(&sortSubsequence, std::ref(right), std::cref(values), middle, end);
-
-   if (leftThread->joinable())
-   {
-      leftThread->join();
-   }
-   if (rightThread->joinable())
-   {
-      rightThread->join();
-   }
-
-   values.clear();
-
-   result = mergeSortedVectors(left, right);
-}
-
-void sortVectors(std::vector<std::vector<unsigned>>& values)
-{
-   int counter = 0;
-   while (counter < values.size())
-   {
-      std::vector<std::unique_ptr<std::thread>> threads;
-      for (int i = 0; i < maxThreadsCount; ++i)
-      {
-         if (counter == values.size())
-         {
-            break;
-         }
-         threads.emplace_back(std::make_unique<std::thread>([&vec = values[counter]](){ std::sort(vec.begin(), vec.end()); }));
-         counter++;
-      }
-
-      for (auto& vp : threads)
-      {
-         if (vp->joinable())
-         {
-            vp->join();
-         }
-      }
-   }
-}
-
-void fillVectorsFromFile(const std::string& fileName, std::vector<std::vector<unsigned>>& values, const long long fileLength)
-{
-   int counter = 0;
    long long startPosition = 0;
    while (startPosition < fileLength)
    {
@@ -238,9 +178,8 @@ void fillVectorsFromFile(const std::string& fileName, std::vector<std::vector<un
             break;
          }
          const long long segment = fileLength - startPosition >= segmentSize ? segmentSize : fileLength - startPosition;
-         threads.emplace_back(std::make_unique<std::thread>(std::bind(&readValuesFromFile, std::cref(fileName), std::ref(values[counter]), startPosition, segment)));
+         threads.emplace_back(std::make_unique<std::thread>(std::bind(&readValuesFromFile, std::cref(fileName), std::ref(values), startPosition, segment)));
          startPosition += segment;
-         counter++;
       }
 
       for (auto& vp : threads)
